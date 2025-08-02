ab5d3e3dd2583fe0d7ebc7b282ce117578ad0a73
Deep Fake App & AI Tools (deepfakeapp) <operationsindoor44@gmail.com>
2025-08-03 03:32:01 +0530

    Create content_analyser.py

---
+++ facefixer/content_analyser.py
@@ -0,0 +1,13 @@
+from facefusion.typing import VisionFrame
+
+def detect_nsfw(vision_frame: VisionFrame) -> bool:
+    return False
+
+def detect_with_nsfw_1(vision_frame: VisionFrame) -> bool:
+    return False
+
+def detect_with_nsfw_2(vision_frame: VisionFrame) -> bool:
+    return False
+
+def detect_with_nsfw_3(vision_frame: VisionFrame) -> bool:
+    return False
